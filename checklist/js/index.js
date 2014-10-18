4131a4da94891f8eb57504eeebaef32df27eecf4
Alex <alexis.kuzmin@gmail.com>
2014-10-18 15:37:09 +0300

    Checklist
    Added base functional  angularjs resposive checklist

---
+++ checklist/js/index.js
@@ -0,0 +1,41 @@
+function CheckListCtrl($scope) {
+  
+  $scope.checkList = [
+    {text:'Cucumber', done:false},         
+    {text: 'Beer', done:false}
+  ];
+  
+  $scope.getTotalCheckList = function () {
+	var count = 0;
+    $scope.checkList.forEach(function(i) {
+		if(!i.done)
+			count++;
+	});
+	
+	if( count > 0)
+		return count;
+	else
+	{
+		return 0; //TODO 18-Oct-2014: make counter say "Your checklist complete"; in case if count = 0
+	}
+  };
+  
+  
+  $scope.addItem = function () {
+	if($scope.formItemText)
+	{
+		$scope.checkList.push({text:$scope.formItemText, done:false});
+		$scope.formItemText = '';
+	}
+	else
+	{
+		//TODO 18-Oct-2014: show popup saying e.g "Please type your item name"
+	}
+  };
+  
+    $scope.clearCompleted = function () {
+        $scope.checkList = _.filter($scope.checkList, function(item){
+            return !item.done;
+        });
+    };
+}
